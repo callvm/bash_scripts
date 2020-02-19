bcc38a44655d12c45c115e66c73199bb6e4484ed
Callum <50365540+callvm@users.noreply.github.com>
2020-02-19 14:23:33 +0200

    Create pivx_depends_old.sh

---
+++ pivx_depends_old.sh
@@ -0,0 +1,19 @@
+# Depends for building older versions of PIVX (v2.x.x)
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y nano htop git
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libminiupnpc-dev
+sudo apt-get install -y autoconf
+sudo apt-get install -y automake unzip
+sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+sudo apt-get install libzmq3-dev
+sudo apt-get install qtcreator
+sudo apt-get install qt5-default
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
