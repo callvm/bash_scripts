2de617ce5780c2a2239d5490e5cbf885f82d66e9
Callum <50365540+callvm@users.noreply.github.com>
2020-03-23 09:16:23 +0200

    Create installJava13.sh

---
+++ installJava13.sh
@@ -0,0 +1,6 @@
+# installs Java 13 and sets it to the default version
+
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt update
+sudo apt install oracle-java13-installer
+sudo apt install oracle-java13-set-default
